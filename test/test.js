5a21c90816de9aecb0fbe068b6f804e3e8bcae59
mnichangxin <mnichangxin@163.com>
2018-11-08 12:35:24 +0800

    add test case

--- test/test.js
+++ test/test.js
@@ -1,22 +1,31 @@
 /**
  * BDD Test
  */
+const path = require('path')
+const fs = require('fs')
 const { expect } = require('chai')
+const rimraf = require('rimraf')
 const checkGit = require('../lib/util/check')
 const getOptions = require('../lib/core/option')
 const clone = require('../lib/core/clone')
 const down = require('../lib/core/down')
 
+const OWNER_REPO_URL = 'mnichangxin/repo-download'
+const GITHUB_SSH_URL = 'git@github.com:mnichangxin/repo-download.git'
+const GITLAB_SSH_URL = 'git@gitlab.com:mnichangxin/repo-download.git'
+const GITHUB_HTTPS_URL = 'https://github.com/mnichangxin/repo-download.git'
+const GITLAB_HTTPS_URL = 'https://gitlab.com/mnichangxin/repo-download.git'
+const OWNER_REPO_URL_CHECKOUT = 'mnichangxin/repo-download#dev'
+const GITHUB_SSH_URL_CHECKOUT = 'git@github.com:mnichangxin/repo-download#dev.git'
+const GITHUB_HTTPS_URL_CHECKOUT = 'https://github.com/mnichangxin/repo-download#dev.git'
+const GITLAB_SSH_URL_CHECKOUT = 'git@gitlab.com:mnichangxin/repo-download#dev.git'
+const CUSTOM_URL = 'https://github.com/mnichangxin/repo-download.git#dev'
+const GITHUB_DOWNLOAD_URL = 'https://github.com/mnichangxin/repo-download/archive/dev.zip'
+const GITLAB_DOWNLOAD_URL = 'https://gitlab.com/mnichangxin/repo-download/repository/archive.zip?ref=dev'
+const MOCK_CLONE_PATH = path.resolve(__dirname, 'mock-clone-path')
+const MOCK_DOWN_PATH = path.resolve(__dirname, 'mock-down-path')
+
 describe('test repo-download', () => {
-    const OWNER_REPO_URL = 'mnichangxin/repo-download'
-    const GITHUB_SSH_URL = 'git@github.com:mnichangxin/repo-download.git'
-    const GITLAB_SSH_URL = 'git@gitlab.com:mnichangxin/repo-download.git'
-    const GITHUB_HTTPS_URL = 'https://github.com/mnichangxin/repo-download.git'
-    const GITLAB_HTTPS_URL = 'https://gitlab.com/mnichangxin/repo-download.git'
-    const OWNER_REPO_URL_CHECKOUT = 'mnichangxin/repo-download#dev'
-    const GITHUB_SSH_URL_CHECKOUT = 'git@github.com:mnichangxin/repo-download#dev.git'
-    const GITHUB_HTTPS_URL_CHECKOUT = 'https://github.com/mnichangxin/repo-download#dev.git'
-    const CUSTOM_URL = 'https://github.com/mnichangxin/repo-download.git#dev'
     describe('test check git', () => {
         it('git is installed/uninstalled', (done) => {
             checkGit((err) => {
@@ -110,7 +119,7 @@ describe('test repo-download', () => {
                 type: 'https'
             }))
         })
-        it('custom', () => {
+        it('custom url', () => {
             const options = getOptions(CUSTOM_URL, {
                 custom: true
             })
@@ -120,8 +129,74 @@ describe('test repo-download', () => {
                 custom: true
             }))
         })
+        it('github ssh url, option type:direct', () => {
+            const options = getOptions(OWNER_REPO_URL_CHECKOUT, {
+                type: 'direct'
+            })
+            expect(options).to.eql(Object.assign(target, defaultOptions, {
+                repo: GITHUB_DOWNLOAD_URL,
+                checkout: 'dev',
+                type: 'direct'
+            }))
+        })
+        it('gitlab ssh url, option type:direct', () => {
+            const options = getOptions(GITLAB_SSH_URL_CHECKOUT, {
+                type: 'direct'
+            })
+            expect(options).to.eql(Object.assign(target, defaultOptions, {
+                repo: GITLAB_DOWNLOAD_URL,
+                checkout: 'dev',
+                type: 'direct'
+            }))
+        })
     })
-    describe('test clone', () => {
-        
+    describe.skip('test clone', function() {
+        const mockClone = (repo, checkout, done) => {
+            clone({
+                repo: repo,
+                checkout: checkout,
+                dist: MOCK_CLONE_PATH
+            }, (err) => {
+                if (err) {
+                    done(err)
+                }
+                const isExists = fs.existsSync(`${MOCK_CLONE_PATH}/repo-download`)
+                expect(isExists).to.be.true
+                done()
+            })
+        }
+        this.timeout(10000)
+        it('ssh clone', (done) => {
+            mockClone(GITHUB_SSH_URL, '', done)
+        })
+        it('https clone', (done) => {
+            mockClone(GITHUB_HTTPS_URL, '', done)
+        })
+        it('ssh clone with checkout', (done) => {
+            mockClone(GITHUB_SSH_URL, 'dev', done)
+        })
+        it('https clone with checkout', (done) => {
+            mockClone(GITHUB_HTTPS_URL, 'dev', done)
+        })
+        afterEach(() => {
+            rimraf.sync(`${MOCK_CLONE_PATH}/repo-download`)
+        })
+    })
+    describe.skip('test down', function() {
+        this.timeout(10000)
+        it('github down', (done) => {
+            down({
+                repo: GITHUB_HTTPS_URL,
+                dist: MOCK_DOWN_PATH
+            }, (err) => {
+                if (err) {
+                    done(err)
+                } else {
+                    const isExists = fs.existsSync(`${MOCK_DOWN_PATH}/*.zip`)
+                    expect(isExists).to.be.true
+                    done()
+                }
+            })
+        })
     })
 })
